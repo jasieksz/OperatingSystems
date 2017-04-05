025030d523345b3aaa8e97eebdf63e96f7b8d5db
jasieksz <jasiek.sznajd@gmail.com>
2017-04-05 02:54:38 -0700

    still not working

--- cw04/zad2/dwa.c
+++ cw04/zad2/dwa.c
@@ -18,9 +18,11 @@ pid_t *children;
 
 void createProc();
 void childAction();
+void childAction2();
 void parentAction();
-void childHandler(int sig);
+void childHandler(int sig, siginfo_t *info, void *context);
 void parentHandler(int sig, siginfo_t *info, void *context);
+void parentHandler2(int sig, siginfo_t *info, void *context);
 
 int main(int argc, char *argv[]){
 
@@ -33,37 +35,56 @@ int main(int argc, char *argv[]){
   k = atoi(argv[2]);
   ktmp = -k;
 
-
-  createProc();
-}
-
-void createProc(){
-  pid_t *children = malloc(sizeof(pid_t)*n);
-  pid_t *procQ = malloc(sizeof(pid_t)*n);
+  pid_t *children = malloc(2*sizeof(pid_t)*n);
+  pid_t *procQ = malloc(2*sizeof(pid_t)*n);
   int pid = 1;
 
   for (int i = 0; i < n; i++){
     if (pid > 0){
-      pid_t pid = fork();
+      pid = fork();
       children[i] = pid;
     } else if (pid < 0){
       perror("Fork failed");
       exit(EXIT_FAILURE);
     }
   }
-  if (pid == 0){ //in child
-    childAction();
-  } else if (pid > 0) { // parent
-    parentAction();
+  if (pid == 0){
+    childAction2();
+
+  } else
+	 parentAction();
+  return 0;
+}
+/*
+void createProc(){
+  pid_t *children = malloc(sizeof(pid_t)*n);
+  pid_t *procQ = malloc(sizeof(pid_t)*n);
+  int pid = 1;
+
+  printf("%s\n","TEST 111111");
+  for (int i = 0; i < n; i++){
+    if (pid > 0){
+      pid = fork();
+      children[i] = pid;
+      printf("NEW CHILD : %d\n",pid);
+    } else if (pid < 0){
+      perror("Fork failed");
+      exit(EXIT_FAILURE);
+    }
   }
+  printf("%s PID : %d\n","TEST 2222",pid);
+  if (pid == 0){
+    parentAction();
+  } else
+	 childAction2();
 }
 
 void childAction(){
 
   time_t startTime,endTime;
   printf("CHILD PID: %d\n",getpid());
-  srand((unsigned int) getpid());
-  sleep(1);
+  //srand((unsigned int) getpid());
+  sleep(3);
   //sleep((unsigned int) rand()%10);
 
   printf("SENDING SIGUSR FROM: %d\n",getpid());
@@ -81,9 +102,46 @@ void childAction(){
   printf("TIME DIFF: %d\n",diffTime);
   exit(diffTime);
 }
+*/
+/*
+*
+*
+*/
+void childAction2(){
 
-void childHandler(int sig){
+  struct sigaction saction;
+  memset(&saction, '\0', sizeof(saction));
+  saction.sa_sigaction = &childHandler;
+  saction.sa_flags = SA_SIGINFO;
+  sigemptyset(&saction.sa_mask);
+
+  time_t startTime,endTime;
+  printf("CHILD PID: %d\n",getpid());
+  //srand((unsigned int) getpid());
+  sleep(3);
+  //sleep((unsigned int) rand()%10);
+
+  printf("SENDING SIGUSR FROM: %d\n",getpid());
+  kill(getppid(), SIGUSR1);
+
+  //signal(SIGUSR1, childHandler);
+  //signal(SIGINT, childHandler);
+  sigaction(SIGUSR1,&saction,NULL);
+  sigaction(SIGINT,&saction,NULL);
+
+  time(&startTime);
+  pause();
+  time(&endTime);
+
+  printf("UNPAUSED PID: %d\n",getpid());
+  int diffTime = (int) difftime(endTime,startTime);
+  printf("TIME DIFF: %d\n",diffTime);
+  exit(diffTime);
+}
+
+void childHandler(int sig, siginfo_t *info, void *context){
   if (sig == SIGUSR1){
+    //kill(getppid(),SIGTSTP);
     kill(getppid(), SIGRTMIN + (rand() % (SIGRTMAX - SIGRTMIN + 1)));
   } else if (sig == SIGINT) {
     exit(1);
@@ -92,6 +150,7 @@ void childHandler(int sig){
 
 void parentAction(){
   struct sigaction saction;
+  memset(&saction, '\0', sizeof(saction));
   saction.sa_sigaction = &parentHandler;
   saction.sa_flags = SA_SIGINFO;
   sigemptyset(&saction.sa_mask);
@@ -102,27 +161,33 @@ void parentAction(){
   for (int i = SIGRTMIN; i <= SIGRTMAX; i++)
     sigaction(i, &saction, NULL);
 
-  while (wait(NULL)){ //wait(NULL) -> wait for any child to terminate
-    if (ktmp == 0)
+  while (wait(NULL)){
+    if (errno == ECHILD)
       break;
   }
+}
+void parentHandler2(int sig, siginfo_t *info, void *context){
+  if (sig == SIGUSR1){
+    printf("CAUGHT SIGUSR1 FROM: %d\n",info->si_pid);
+  } else if (sig == SIGINT){
+    printf("CAUGHT SIGINT FROM: %d\n",info->si_pid);
+  } else if(sig == SIGTSTP){
+    printf("CAUGHT SIGTSTP: %d\n",info->si_pid);
+  } else {
+    printf("%s\n","COS INNEGO AABASJFAKSF");
+  }
+
 }
 
 void parentHandler(int sig, siginfo_t *info, void *context){
   if (sig == SIGUSR1){
     printf("CAUGHT SIGUSR1 FROM: %d\n",info->si_pid);
     procQ[ktmp + k] = info->si_pid;
-    ktmp += 1;
-
+    ktmp++;
     if (ktmp == 0){
-      kill(info->si_pid, SIGUSR1);
-    } else {
-      ktmp += 1;
-      if (ktmp == 0){
-        for (int i=0; i < k; i++){
-          kill(procQ[i], SIGUSR1);
-          waitpid(procQ[i],NULL,0);
-        }
+      for (int i=0; i < k; i++){
+        kill(procQ[i], SIGUSR1);
+        waitpid(procQ[i],NULL,0);
       }
     }
   } else if (sig == SIGINT){
