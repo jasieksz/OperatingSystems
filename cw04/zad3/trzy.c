e4f5f84424d203b97d282759fcdc5d6c7f6dbaff
jasieksz <jasiek.sznajd@gmail.com>
2017-04-13 05:51:18 -0700

    done

--- cw04/zad3/trzy.c
+++ cw04/zad3/trzy.c
@@ -1,6 +1,6 @@
 #define _XOPEN_SOURCE 600
-#define _POSIX_C_SOURCE 199309L
-#define _GNU_SOURCE_
+#define _POSIX_C_SOURCE 200809L
+//#define _GNU_SOURCE_
 
 #include <stdlib.h>
 #include <string.h>
@@ -20,7 +20,7 @@ void parentHandler(int sig, siginfo_t *info, void *context);
 
 
 int L,type,counter,sent;
-pid_t childPid;
+pid_t child;
 
 int main(int argc, char *argv[]){
 
@@ -32,28 +32,69 @@ int main(int argc, char *argv[]){
   L = atoi(argv[1]);
   type = atoi(argv[2]);
   counter = 0;
+
+  struct sigaction paction;
+  paction.sa_sigaction = &parentHandler;
+  paction.sa_flags = SA_RESTART;
+  sigemptyset(&paction.sa_mask);
+
+  sigaction(SIGUSR1, &paction, NULL);
+  sigaction(SIGINT, &paction, NULL);
+
+
   pid_t pid = fork();
+
   if (pid == 0){
-    childAction();
+    struct sigaction saction;
+    saction.sa_sigaction = &childHandler;
+    saction.sa_flags = SA_SIGINFO | SA_RESTART;
+    sigemptyset(&saction.sa_mask);
+
+    sigaction(SIGUSR1, &saction, NULL);
+    sigaction(SIGUSR2, &saction, NULL);
+    sigaction(SIGINT, &saction, NULL);
+    sigaction(SIGRTMIN, &saction, NULL);
+    sigaction(SIGRTMIN+1, &saction, NULL);
+    sleep(1);
   } else{
-    childPid = pid;
-    parentAction(pid);
+    sleep(1);
+    child = pid;
+
+      union sigval myVal;
+
+      for (int i=0;i < L;i++){
+
+        switch(type) {
+          case (1):
+            kill(child,SIGUSR1);
+            break;
+          case (2):
+            sigqueue(child, SIGUSR1, myVal);
+            break;
+          case (3):
+            kill(child,SIGRTMIN);
+            break;
+        }
+      }
+
+
+      switch (type) {
+        case (1):
+          kill(child,SIGUSR2);
+          break;
+        case (2):
+          sigqueue(child, SIGUSR2, myVal);
+          break;
+        case (3):
+          kill(child,SIGRTMIN+1);
+          break;
+      }
+      sleep(1);
+
+      printf("SENT SIGUSR1 : %d CAUGHT %d\n",L,counter);
   }
 }
 
-void childAction(){
-  struct sigaction saction;
-  saction.sa_sigaction = &childHandler;
-  saction.sa_handler = &childHandler;
-  saction.sa_flags = SA_SIGINFO | SA_RESTART;
-  sigemptyset(&saction.sa_mask);
-
-  sigaction(SIGUSR1, &saction, NULL);
-  sigaction(SIGUSR2, &saction, NULL);
-  sigaction(SIGINT, &saction, NULL);
-  sigaction(SIGRTMIN, &saction, NULL);
-  sigaction(SIGRTMAX, &saction, NULL);
-}
 
 void childHandler(int sig, siginfo_t *info, void *context){
   if (sig == SIGUSR1){
@@ -65,67 +106,18 @@ void childHandler(int sig, siginfo_t *info, void *context){
   } else if (sig == SIGRTMIN){
     counter++;
     kill(info->si_pid,SIGUSR1);
-  } else if (sig == SIGRTMAX){
-    printf("CAUGHT SIGRTMAX, COUNTER : %d\n",counter);
+  } else if (sig == SIGRTMIN+1){
+    printf("CAUGHT SIGRTMIN+1, COUNTER : %d\n",counter);
     exit(0);
   } else if (sig == SIGINT)
     exit(1);
 }
 
-void parentAction(pid_t child){
-  struct sigaction saction;
-  saction.sa_sigaction = &parentHandler;
-  saction.sa_handler = &parentHandler;
-  saction.sa_flags = SA_RESTART;
-  sigemptyset(&saction.sa_mask);
-
-  sigaction(SIGUSR1, &saction, NULL);
-  sigaction(SIGINT, &saction, NULL);
-
-  union sigval myVal;
-
-  for (int i=0;i < L;i++){
-
-    switch(type) {
-      case (1):
-        kill(child,SIGUSR1);
-        break;
-      case (2):
-        sigqueue(child, SIGUSR1, myVal);
-        break;
-      case (3):
-        kill(child,SIGRTMIN);
-        break;
-    }
-  }
-
-
-  switch (type) {
-    case (1):
-      kill(child,SIGUSR2);
-      break;
-    case (2):
-      sigqueue(child, SIGUSR2, myVal);
-      break;
-    case (3):
-      kill(child,SIGRTMAX);
-      break;
-  }
-
-  while (wait(NULL)){
-    if (errno == ECHILD)
-      break;
-  }
-
-  printf("SENT SIGUSR1 : %d CAUGHT %d\n",L,counter);
-}
-
 void parentHandler(int sig, siginfo_t *info, void *context){
-  printf("%s\n","PARENT HANDLER" );
   if (sig == SIGUSR1)
     counter++;
   else if (sig == SIGINT){
-    kill(childPid, SIGINT);
+    kill(child, SIGINT);
     exit(1);
   }
 }
