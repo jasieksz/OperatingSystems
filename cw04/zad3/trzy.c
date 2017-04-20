7adb5deb65c3359283457ba1b49d183d87c54a23
Jasiek <jasiek.sznajd@gmail.com>
2017-04-20 18:43:25 +0200

    fix

--- cw04/zad3/trzy.c
+++ cw04/zad3/trzy.c
@@ -47,51 +47,51 @@ int main(int argc, char *argv[]){
   if (pid == 0){
     struct sigaction saction;
     saction.sa_sigaction = &childHandler;
-    saction.sa_flags = SA_SIGINFO | SA_RESTART;
+    saction.sa_flags = (SA_SIGINFO | SA_RESTART);
     sigemptyset(&saction.sa_mask);
+    sigaddset(&saction.sa_mask, SIGRTMAX);
 
     sigaction(SIGUSR1, &saction, NULL);
     sigaction(SIGUSR2, &saction, NULL);
     sigaction(SIGINT, &saction, NULL);
     sigaction(SIGRTMIN, &saction, NULL);
-    sigaction(SIGRTMIN+1, &saction, NULL);
-    sleep(1);
+    sigaction(SIGRTMAX, &saction, NULL);
+
+    while(1)
+      sigsuspend(0);
+
+    printf("%s\n","KONIEC DIZECKA");
   } else{
     sleep(1);
     child = pid;
-
-      union sigval myVal;
-
+    union sigval myVal;
       for (int i=0;i < L;i++){
 
         switch(type) {
           case (1):
-            kill(child,SIGUSR1);
+            kill(pid,SIGUSR1);
             break;
           case (2):
-            sigqueue(child, SIGUSR1, myVal);
+            sigqueue(pid, SIGUSR1, myVal);
             break;
           case (3):
-            kill(child,SIGRTMIN);
+            kill(pid,SIGRTMIN);
             break;
         }
       }
-
-
+      sleep(1);
       switch (type) {
         case (1):
-          kill(child,SIGUSR2);
+          kill(pid,SIGUSR2);
           break;
         case (2):
-          sigqueue(child, SIGUSR2, myVal);
+          sigqueue(pid, SIGUSR2, myVal);
           break;
         case (3):
-          kill(child,SIGRTMIN+1);
+          kill(pid,SIGRTMAX);
           break;
       }
-      sleep(1);
 
-      printf("SENT SIGUSR1 : %d CAUGHT %d\n",L,counter);
   }
 }
 
@@ -106,8 +106,8 @@ void childHandler(int sig, siginfo_t *info, void *context){
   } else if (sig == SIGRTMIN){
     counter++;
     kill(info->si_pid,SIGUSR1);
-  } else if (sig == SIGRTMIN+1){
-    printf("CAUGHT SIGRTMIN+1, COUNTER : %d\n",counter);
+  } else if (sig == SIGRTMAX){
+    printf("CAUGHT SIGRTMAX, COUNTER : %d\n",counter);
     exit(0);
   } else if (sig == SIGINT)
     exit(1);
@@ -117,7 +117,8 @@ void parentHandler(int sig, siginfo_t *info, void *context){
   if (sig == SIGUSR1)
     counter++;
   else if (sig == SIGINT){
-    kill(child, SIGINT);
-    exit(1);
+    //kill(child, SIGINT);
+    //exit(1);
+    printf("%s\n","PARENT RECEIVED SIGINT" );
   }
 }
