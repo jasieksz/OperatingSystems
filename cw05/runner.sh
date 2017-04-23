f61aba968d45dadbf4ce3dba36f6448badaea0f9
jasieksz <jasiek.sznajd@gmail.com>
2017-04-23 05:45:41 -0700

    zad 1 done

--- cw05/runner.sh
+++ cw05/runner.sh
@@ -8,3 +8,6 @@ cmake -DCMAKE_INSTALL_PREFIX=../bin ..
 make
 make install
 
+#cp /home/student/Desktop/OperatingSystems/cw05/in /home/student/Desktop/OperatingSystems/cw05/bin/zad1
+
+
--- cw05/zad1/pipes.c
+++ cw05/zad1/pipes.c
@@ -1,8 +1,5 @@
-#define _POSIX_C_SOURCE 2
-#define _XOPEN_SOURCE 600
 #define _GNU_SOURCE
 
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,64 +7,91 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define CMDS 20
+#define ARGS 5
+#define PDEL "|"
+#define WDEL " \n"
+int lastCmd = 0;
 
-void parse(FILE *filePointer);
-char **splitString(char *line, int *counter);
+void parse(char *line);
+char **splitString(char *line, char *deli, int *counter);
+void execute(char *cmd, int *fd, int in);
 
 int main(int argc, char *argv[]){
-
-  if (argc != 2){
-    printf("Bledna ilosc argumentow\n");
-    exit(EXIT_FAILURE);
-  }
-  char *fileName;
-  fileName = argv[1];
-  FILE *filePointer;
-  if(!(filePointer = fopen(fileName,"r"))){
-      perror("Opening file failed\n");
-      exit(EXIT_FAILURE);
-  }
-  parse(filePointer);
-  fclose(filePointer);
-  return 0;
-}
-
-void parse(FILE *filePointer){
   char *line = NULL;
   size_t len = 0;
   ssize_t read;
-  while ((read = getline(&line, &len, filePointer)) != -1){ // -1 <=> EOF
-    int counter = 0;
-    char **args = splitString(line,&counter); // args=[prog,arg1,arg2,|,prog2,arg1]
-    char **progArgs = malloc(5 * 12 * sizeof(char));
-    int i = 0;
-    while (i < counter){
-      int j = 0;
-      while (i < counter && strcmp(args[i],"|") != 0){
-        progArgs[j] = args[i];
-        i++; j++;
-      }
-      for (int c=0;c<j;c++)
-        printf("%s ",progArgs[c]);
-      printf("\n");
-      progArgs[j] = NULL;
-      i++;
-      printf("EXECUTE\n\n"); //TODO execute this program
-    }
-  }
+  while ((read = getline(&line, &len, stdin)) != -1)
+    parse(line);
+
+  return 0;
 }
 
-char **splitString(char *line, int *counter){
-  char **args = malloc(80*sizeof(char *)); //20 pipeow po 3 argumenty
+void parse(char *line){
+  //printf("%s\n","TEST 2 - PARSE PARSE PARSE");
+  int cmdCounter; cmdCounter = 0;
+  char **cmds = splitString(line,PDEL,&cmdCounter);
+
+  int fd[2]; // odczyt , zapis
+  pipe(fd);
+  int prevOut = STDIN_FILENO;
+
+  for (int i=0; i < cmdCounter; i++){ //foreach cmd -> pipe
+    pipe(fd);
+    if (i == cmdCounter-1)
+      lastCmd = 1;
+    execute(cmds[i],fd,prevOut);
+    prevOut = fd[0];
+    close(fd[1]);
+  }
+  close(fd[0]);
+  free(cmds);
+}
+
+void execute(char *cmd, int *fd, int in){
+  int argCounter; argCounter = 0;
+  char **args = splitString(cmd,WDEL,&argCounter);
+
+  switch(fork()){
+    case -1:
+      perror("Fork failed");
+      exit(1);
+    case 0: // child
+      if (in != -1)
+        dup2(in,0);
+      if (!lastCmd)
+        dup2(fd[1],1);
+      if (execvp(args[0],args) == -1){
+        perror("Executing cmd failed");
+        exit(1);
+      }
+    default: { //parent
+      close(fd[1]);
+      dup2(fd[0],0);
+      wait(NULL);
+      if (in != -1)
+        close(in);
+    }
+  }
+  free(args);
+}
+
+char **splitString(char *line, char *deli, int *counter){
+  int number = (deli == PDEL) ? CMDS : ARGS;
+  //printf("SPLIT STRING NUMBER : %d\n",number);
+  char **args = malloc(number+1 * sizeof(char *));
   char *token;
   int i = 0;
-  token = strtok(line," \n");
-  while (token && i < 80){
+
+  token = strtok(line,deli);
+  while (token && i < number){
     args[i] = malloc((strlen(token)+1)*sizeof(char));
     strcpy(args[i],token);
     i++;
-    token = strtok(NULL," \n");
+    token = strtok(NULL,deli);
   }
+  args[i] = NULL;
   *counter = i;
+  //printf("SPLIT STRING COUNTER : %d\n",i);
   return args;
 }
